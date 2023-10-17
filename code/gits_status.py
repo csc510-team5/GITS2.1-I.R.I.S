082eaf201887b1b04b33f4461f7ad541be5ec140
Sana Mahmoud <simahmou@ncsu.edu>
2023-10-17 14:26:10 -0400

    removed get_recent-gmrs

--- code/gits_status.py
+++ code/gits_status.py
@@ -2,8 +2,6 @@
 
 from subprocess import PIPE
 import subprocess
-import requests
-import json
 
 
 def gits_status(args):
@@ -18,7 +16,6 @@ def gits_status(args):
         process1 = subprocess.Popen(status_cmd, stdout=PIPE, stderr=PIPE)
         stdout, stderr = process1.communicate()
         print(stdout.decode("UTF-8"))
-        get_recent_gmrs()
 
     except Exception as e:
         print("ERROR: gits status command caught an exception")
@@ -26,59 +23,4 @@ def gits_status(args):
         return False
 
     return True
-
-
-def get_recent_gmrs():
-    target_branch = "main"
-    graphql_url = "https://api.github.com/graphql"
-    query = """
-{
-        repository(owner: "Trinea", name: "android-open-project") {
-            pullRequests(first: 100,states: OPEN, orderBy: { field: CREATED_AT, direction: DESC }) {
-                nodes {
-                    number
-                    title
-                    author {
-                        login
-                    }
-                    files(first: 100) {  # Adjust 'first' to limit the number of files retrieved
-                        nodes {
-                            path
-                            additions
-                            deletions
-
-
-                        }
-                    }
-                    baseRef{
-                        target{
-                          oid
-                        }
-                    }
-                    headRef{
-                         target{
-                          oid
-                        }
-                    }
-                }
-            }
-        }
-    }
-    """
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer Your-Git-Token",
-    }
-
-    data = {"query": query}
-    response = requests.post(graphql_url, headers=headers, data=json.dumps(data))
-    if response.status_code == 200:
-        response_data = response.json()  # Parse the JSON response
-        print(json.dumps(response_data, indent=4))  # Print the response data with indentation
-
-
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        print(response.text)
-
 
