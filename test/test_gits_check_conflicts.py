4a3085c20b8040d6ae73fea2fc7a99fc7264d5ab
Joshua Joseph <joshjoseph97@gmail.com>
2023-10-18 23:58:24 -0400

    :bug: fix conflicts test

--- test/test_gits_check_conflicts.py
+++ test/test_gits_check_conflicts.py
@@ -3,11 +3,11 @@ import sys
 
 sys.path.insert(1, os.getcwd())
 
-from gits_check_conflicts import modified_files, merge_base
+from gits_check_conflicts import git_modified_files, git_merge_base
 
 def test_modified_files():
     target_branch = "master"
-    merge_base_sha = merge_base(target_branch)
-    files = modified_files(merge_base_sha)
+    merge_base_sha = git_merge_base(target_branch)
+    files = git_modified_files(merge_base_sha)
     print(files)
     assert isinstance(files, list)
