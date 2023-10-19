4db7db0a304ca6db7161ef41a1e44256c439e31f
Sana Mahmoud <simahmou@ncsu.edu>
2023-10-19 15:22:00 -0400

    added tests to test file

--- test/test_gits_check_conflicts.py
+++ test/test_gits_check_conflicts.py
@@ -3,7 +3,8 @@ import sys
 
 sys.path.insert(1, os.getcwd())
 
-from gits_check_conflicts import modified_files, merge_base
+from gits_check_conflicts import git_modified_files, git_merge_base, check_conflicts
+from mock import patch, Mock
 
 def test_modified_files():
     target_branch = "master"
@@ -11,3 +12,35 @@ def test_modified_files():
     files = modified_files(merge_base_sha)
     print(files)
     assert isinstance(files, list)
+ 
+@patch("subprocess.Popen") 
+def test_check_conflicts_pass(mock_var):
+    """
+    Function to test gits check conflicts, success case
+    """
+    mocked_pipe = Mock()
+    attrs = {'communicate.return_value': ('output'.encode('UTF-8'), 'error'), 'returncode': 0}
+    mocked_pipe.configure_mock(**attrs)
+    mock_var.return_value = mocked_pipe
+    test_result = check_conflicts()
+    if test_result:
+        assert True, "Normal Case"
+    else:
+        assert False
+
+    
+@patch("subprocess.Popen") 
+def test_check_conflicts_fail(mock_var):
+    """
+    Function to test gits check conflicts, success case
+    """
+    mocked_pipe = Mock()
+    attrs = {'communicate.return_value': ('output', 'error'), 'returncode': 0}
+    mocked_pipe.configure_mock(**attrs)
+    mock_var.return_value = mocked_pipe
+    test_result = check_conflicts()
+    if test_result:
+        assert True, "Normal Case"
+    else:
+        assert False
+
