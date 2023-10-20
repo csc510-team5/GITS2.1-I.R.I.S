21edca980ae5611aafef3be62d99d9c6fa0ea323
Paul <ukpaulbd@gmail.com>
2023-10-19 20:07:53 -0400

    Testing the length of modified list

--- test/test_gits_check_conflicts.py
+++ test/test_gits_check_conflicts.py
@@ -10,3 +10,12 @@ def test_modified_files():
     merge_base_sha = git_merge_base(target_branch)
     files = git_modified_files(merge_base_sha)
     assert isinstance(files, list)
+
+def test_modified_files_length():
+    target_branch = "master"
+    merge_base_sha = git_merge_base(target_branch)
+    files = git_modified_files(merge_base_sha)
+    assert len(files) > 0
+
+
+
